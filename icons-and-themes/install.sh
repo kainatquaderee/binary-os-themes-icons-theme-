8ea7d79b216b6018b196f9bf04d17c6cdf6047ff
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-22 17:50:18 +0600

    sdasd

--- icons-and-themes/install.sh
+++ icons-and-themes/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ./icons-and-themes
-cp -R  ./.local/share/themes/* /usr/share/themes
-cp -R ./.local/share/icons/* /usr/share/icons
+echo $password | sudo -S cp -R  ./.local/share/themes/* /usr/share/themes
+echo $password | sudo -S  cp -R ./.local/share/icons/* /usr/share/icons
 cp -R ./.local/share/iccon/* ~/.local/share/icons
